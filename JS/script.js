5c5c9968958287099e026fa9542574d2db069eab
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-20 22:15:50 +0200

    Aggiunta classe bomb sul css e provato a creare sistema per confrontare i numeri bomba con il numero cliccato

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,35 @@
 const buttonEl = document.querySelector('button');
 const gridEl = document.querySelector('section.griglia');
+
+function getRandom (max, min){
+    return Math.floor(Math.random() * ((max + 1)- min)) + min;
+}
+
+const bomb = []
+let i = 0;
 buttonEl.addEventListener('click',function (){
     for(let i = 0; i < 100; i++){
         const articleEl = document.createElement('article');
         articleEl.addEventListener('click', function(){
-            articleEl.classList.add('active')
+            for(let i = 0; i < bomb.length; i++){
+                if(i !== bomb.indexOf(i)){
+                    articleEl.classList.add('active')
+                }
+                else{
+                    articleEl.classList.add('bomb')
+                }
+            }
             console.log(i+1)
         })
         gridEl.appendChild(articleEl);
         articleEl.append(i+1)
     }
+    while(i < 16){
+        let numberRand = getRandom(100, 1)
+        if(numberRand != bomb.findIndex(numberRand)){
+            bomb.push(numberRand);
+            i++;
+        }
+    }
+    console.log(bomb)
 })
-function getRandom (max, min){
-    return Math.floor(Math.random() * ((max + 1)- min)) + min;
-}
-const bomb = []
-let i = 0;
-while(i < 16){
-    let numberRand = getRandom(100, 1)
-    if(numberRand == bomb.indexOf(numberRand)){
-        i = i
-    }
-    else{
-        bomb.push(numberRand);
-        i++;
-    }
-}
-
-
-console.log(bomb)
